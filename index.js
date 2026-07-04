18f68ca32fb06b8233deced92d2e044a7df1b682
cfardin <cfardin51@gmail.com>
2026-07-04 20:31:35 +0600

    fixed dns issue

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+const client = new MongoClient(process.env.MONGODB_URI);
 
+async function connectToMongoDB() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+    return client;
+  } catch (err) {
+    console.dir(err);
+  }
+}
+
+async function disconnectFromMongoDB() {
+  await client.close();
+}
+
+connectToMongoDB();
 
 app.get('/', (req, res) => {
-    res.send("Server is running");
-})
+  res.send("Server is running");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = { connectToMongoDB, disconnectFromMongoDB };
